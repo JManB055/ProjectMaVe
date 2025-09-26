be3854a2c995d406a2a508bda9697d284fbf7843
yhr0x43 <yhr0x43@gmail.com>
2025-09-26 10:08:43 -0500

    empty middleware template

---
+++ Middleware/AuthenticationMiddleware.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ProjectMaVe.Middleware
+{
+    public class AuthenticationMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await next(context);
+        }
+    }
+}
