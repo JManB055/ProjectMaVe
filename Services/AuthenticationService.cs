99bfe6a12995fb8286e9189cfc84a1f43b669151
tclayton003 <tyler.clayton@okstate.edu>
2025-09-29 10:15:50 -0500

    GetCurrentUser with placeholders

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -5,9 +5,24 @@ namespace ProjectMaVe.Services;
 
 public class AuthenticationService : IAuthenticationService
 {
+	/*
+	GetCurrentUser will return an object of the User class,
+	which is defined in ~/Models/User.cs
+	
+	The user has the following parameters to define them:
+	Guid UserID - the user ID
+	string PassHash - the hash of the password
+	string PassSalt - salting the hash
+	string FirstName - user first name (nullable)
+	string LastName - user last name (nullable)
+	string Email - user email (nullable)
+	*/
     public User GetCurrentUser()
     {
-        throw new NotImplementedException();
+		//placeholder parameters
+		User currentUser = new User(Guid.NewGuid(), "hash", "salt", "firstname", "lastname", "email");
+		
+        return currentUser;
     }
 
     public string GetUserName(int id)
