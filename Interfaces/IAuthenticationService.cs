cace7534764c924414ef75a91cdb14e894e671ae
yhr0x43 <yhr0x43@gmail.com>
2025-09-29 09:53:02 -0500

    empty GetCurrentUser

--- Interfaces/IAuthenticationService.cs
+++ Interfaces/IAuthenticationService.cs
@@ -1,6 +1,10 @@
-﻿namespace ProjectMaVe.Interfaces;
+﻿using ProjectMaVe.Models;
+
+namespace ProjectMaVe.Interfaces;
 
 public interface IAuthenticationService
 {
     public string GetUserName(int id);
+
+    public User GetCurrentUser();
 }
--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -1,9 +1,15 @@
 ﻿using ProjectMaVe.Interfaces;
+using ProjectMaVe.Models;
 
 namespace ProjectMaVe.Services;
 
 public class AuthenticationService : IAuthenticationService
 {
+    public User GetCurrentUser()
+    {
+        throw new NotImplementedException();
+    }
+
     public string GetUserName(int id)
     {
         return "Admin";
