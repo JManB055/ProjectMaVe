71bfc17f5de23e8c039d264b0425992b2fec915b
tclayton003 <tyler.clayton@okstate.edu>
2025-10-20 10:07:40 -0500

    Fitbit API basic setup

--- Interfaces/IFitbitAuthenticator.cs
+++ Interfaces/IFitbitAuthenticator.cs
@@ -1,6 +1,12 @@
-﻿namespace ProjectMaVe.Interfaces
+﻿using ProjectMaVe.Models;
+
+namespace ProjectMaVe.Interfaces
 {
     public interface IFitbitAuthenticator
     {
+        string GetAuthorizationUrl();
+        Task<Token> GetTokenAsync(string code);
+        Task<Token> RefreshTokenAsync(string token);
+
     }
 }
