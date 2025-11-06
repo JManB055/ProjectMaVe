35e2edb5d8592f6d8db8451258b9feb247be60c5
Joshua Wyckoff <joshua.wyckoff@okstate.edu>
2025-11-05 18:46:42 -0600

    Added ability to delete widgets when editing them.

--- wwwroot/js/widgets.js
+++ wwwroot/js/widgets.js
@@ -1,81 +1,139 @@
 /*
-*   Get widgets from the database and display them in HTML
-*   First, get the dashboard container (Div that holds widgets) as 'container'
-*   Next, get widget information from the database
-*   Next, format the HTML based on widgets
-*   Last, set the HTML code of 'container'
+*   Initialize Global variables:
+*       'container' is the dashboard container for all the widgets; all widgets are rendered within this object.
+*       'editSaveBtn' is the button that changes the 'draggable' state.
+*       'deleteBtns' is an array storing all delete buttone (used so that we can hide the delete buttons)
+*       'widgets' is the array of widget metadata used to render the widgets with the right place, size, and data.
+*       'draggies' is the array of Draggabilly objects tied to the widgets. This is neccessary for draggaing capabilities.
+*       'draggable' is a boolean to track the state of the dashboard (editing mode vs view mode)
 */
-// Get the dasboard container as 'container'
 const container = document.querySelector('.dashboard_container');
+const editSaveBtn = document.getElementById('editSaveBtn');
+var deleteBtns;
+var widgets = [];
+var draggies = [];
+var draggable = true;
 
-// Get widget information from the database (For now, we have default widgets.)
+
+/*
+*   Set up the initial page rendering
+*       1) Get widget information from the database using 'getWidgets' function
+*       2) Render the widgets in HTML using the 'renderWidgets' function.
+*/
+
+// Set default widgets for testing
+// TODO: Once we can pull from database, we no longer need default widgets; delete following 7 lines of code.
 var defaultWidgets = [
     { userID: '0', x: 50, y: 50, w: 2, h: 2, type: 'Test Widget' },
     { userID: '0', x: 650, y: 50, w: 1, h: 1, type: 'Test Widget' },
     { userID: '0', x: 650, y: 350, w: 1, h: 1, type: 'Test Widget' },
     { userID: '0', x: 950, y: 50, w: 1, h: 2, type: 'Test Widget' }
 ];
+widgets = defaultWidgets;
 
-// TODO: Update this to pull from database
-var widgets = defaultWidgets;
+// Get widget information from database
+getWidgets()
 
-
-// Format the HTML based on widgets
-var htmlString = '';
-
-for (var i = 0; i < defaultWidgets.length; i++) {
-    // TODO: Implement different widget types
-    htmlString += '<div class="widget widget_card size-' + widgets[i].w + 'x' + widgets[i].h + '"><p>Drag Me Around!</p></div>'
-}
-
-// Set the HTML code of 'container' based on previous formatting
-container.innerHTML = htmlString;
-
-/*
-*   Initialize Boolean 'draggable' to track if widgets are draggable
-*   And gets the button object 'editSaveBtn' which needs to change based on 'draggable'
-*/
-var draggable = true;
-const editSaveBtn = document.getElementById('editSaveBtn');
-
-
-/*
-*   Initialize draggabilly elements
-*   First, get all qualifying objects from the HTML as 'elemnents'
-*   Next, add all 'elements' to 'draggies' as draggabilly objects
-*   Finally, set the postions based on the saved positions
-*/
-// Get all qualifying objects from the HTML as 'elements'
-var elements = document.querySelectorAll('.widget', {});
-var draggies = []
-
-// Add all 'elements' to 'draggies' as graggabilly objects
-for (var i = 0; i < elements.length; i++) {
-    var draggableElem = elements[i];
-    var draggie = new Draggabilly(draggableElem, {
-        grid: [50, 50],
-        containment: ".dashboard_container"
-    });
-    draggies.push(draggie)
-}
-
-// Set the postions based on saved positions
-for (var i = 0; i < widgets.length; i++) {
-    var draggable = widgets[i];
-    x = widgets[i].x;
-    y = widgets[i].y;
-    draggies[i].setPosition(x, y);
-}
+// Render the widgets in HTML
+renderWidgets()
 
 // Set the dashboard to be static
 toggleDraggble();
 
+
 /*
-*   Toggles the draggable state of the widgets
-*   If they are currently draggable, then we disable draggabilly, change the 'editSaveBtn' to Edit, and save the new positions to database.
-*   If they are not currently draggable, then we enable draggabilly and change the 'editSaveBtn' to Save.
-*   Finally, toggle to boolean 'draggable' variable to track state.
-*   Called by 'editSaveBtn' and after setting up the page.
+*   Adds EventListener for the delete button. On Click:
+*       1) Save current widget positions
+*       2) Delete widget from 'widgets' array
+*       3) Save 'widgets' array to Database
+*       4) Re-Render the 'widgets' array
+*/
+container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete-btn')) {
+        // Save current widget positions
+        for (var i = 0; i < draggies.length; i++) {
+            widgets[i].x = draggies[i].position.x;
+            widgets[i].y = draggies[i].position.y;
+        }
+
+        // Delete widget from 'widgets' array
+        const index = parseInt(event.target.dataset.index);
+        widgets.splice(index, 1);
+
+        // Save 'widgets' array to Database
+        saveWidgets();
+
+        // Re-Render the 'widgets' array
+        renderWidgets();
+    }
+});
+
+/*
+*   Function to render widgets in HTML
+*       1) Initialize string variable to store HTML code (for formatting)
+*       2) Write HTML code to 'htmlString' based on widget metadata
+*       3) Set the HTML code of 'container'
+*       4) Save all delete buttons
+*
+*   And initialize Draggabilly objects
+*       4) Get all qualifying objects from the HTML as 'elements'
+*       5) Add all 'elements' to 'draggies' as Draggabilly objects
+*       6) Set the widget positions
+*/
+function renderWidgets() {
+    // Initialize string variable to store HTML code
+    var htmlString = '';
+
+    // Write HTML code to 'htmlString' based on widget metadata (in 'widgets' array)
+    for (var i = 0; i < defaultWidgets.length; i++) {
+        // TODO: Implement different widget types
+        htmlString += '<div class="widget widget_card size-' + widgets[i].w + 'x' + widgets[i].h + '">' +
+            '<button class="delete-btn" data-index="' + i + '">X</button>' +
+            '<p>Drag Me Around!</p></div>';
+    }
+
+    // Set the HTML code of 'container' based on previous formatting
+    container.innerHTML = htmlString;
+
+    // Get all qualigying delete buttons and save to deleteBtns
+    deleteBtns = document.querySelectorAll(".delete-btn", {});
+
+    // Get all qualifying objects from the HTML as 'elements'
+    var elements = document.querySelectorAll('.widget', {});
+    draggies = []
+
+    // Add all 'elements' to 'draggies' as graggabilly objects
+    for (var i = 0; i < elements.length; i++) {
+        var draggableElem = elements[i];
+        var draggie = new Draggabilly(draggableElem, {
+            grid: [50, 50],
+            containment: ".dashboard_container"
+        });
+        draggies.push(draggie)
+    }
+
+    // Set the postions based on saved positions
+    for (var i = 0; i < widgets.length; i++) {
+        var draggable = widgets[i];
+        x = widgets[i].x;
+        y = widgets[i].y;
+        draggies[i].setPosition(x, y);
+    }
+}
+
+/*
+*   Fucntion to toggle the draggable state of the widgets
+*       1) If they are currently draggable, then: 
+*           1) Disable draggabilly
+*           2) Change the 'editSaveBtn' to Edit
+*           3) Save the new positions to database.
+*
+*       2) If they are not currently draggable, then:
+*           1) Enable draggabilly
+*           2) Change the 'editSaveBtn' to Save.
+*
+*       3) Hide/Show delete buttons based on draggable status
+*       4) Toggle to boolean 'draggable' variable to track state.
 */
 function toggleDraggble() {
     if (draggable) {
@@ -84,25 +142,38 @@ function toggleDraggble() {
             // Disable draggabilly for each object
             draggies[i].disable();
 
-            // Update 'editSaveBtn'
-            editSaveBtn.textContent = "Edit";
-
             // Update database with new positions
             widgets[i].x = draggies[i].position.x;
             widgets[i].y = draggies[i].position.y;
-            // TODO: Save the widgets array to the database.
         }
+
+        // Update 'editSaveBtn'
+        editSaveBtn.textContent = "Edit";
+        saveWidgets();
     }
     else {
         // If not currently draggable
         for (var i = 0; i < draggies.length; i++) {
             // Enable draggabilly for each object
             draggies[i].enable();
-
-            // Update the 'editSaveButton'
-            editSaveBtn.textContent = "Save";
         }
+        // Update the 'editSaveButton'
+        editSaveBtn.textContent = "Save";
     }
-    // Toggle editSaveBtn
+
+    for (var i = 0; i < deleteBtns.length; i++) {
+        deleteBtns[i].hidden = draggable;
+    }
     draggable = !draggable;
+}
+
+// TODO: Write function to get the widgets. It doesn't need return, just set existing 'widgets' array to data from database.
+function getWidgets() {
+
+    //widgets = widgetsFromDatabase
+}
+
+// TODO: Write function to save widgets to the database (Alex, write save code here to simply save the entire widgets array to database)
+function saveWidgets() {
+
 }
