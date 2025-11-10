6968fca4f9ea3bcee95423913ef4a48d2ba7a30b
tclayton003 <tclayton2003@gmail.com>
2025-11-10 10:38:51 -0600

    moved to C drive

--- Pages/AI.cshtml.cs
+++ Pages/AI.cshtml.cs
@@ -5,24 +5,23 @@ using ProjectMaVe.Interfaces;
 
 namespace ProjectMaVe.Pages
 {
-    public class AITestModel : PageModel
-    {
-        [BindProperty]
-        required public string Prompt { get; set; }
+	public class AITestModel : PageModel
+	{
+		[BindProperty]
+		required public string Prompt { get; set; }
 
-        required public string Message { get; set; }
+		required public string Message { get; set; }
 
-        private readonly IAIService _aiService;
-        public AITestModel(IAIService aiService)
-        {
-            _aiService = aiService;
-        }
+		private readonly IAIService _aiService;
+		public AITestModel(IAIService aiService)
+		{
+			_aiService = aiService;
+		}
 
-        public async Task OnPostAsync()
-        {
-
-            Message = await _aiService.CallAIAsync(Prompt);
-        }
-    }
+		public async Task OnPostAsync()
+		{
+			Message = await _aiService.CallAIAsync(Prompt);
+		}
+	}
 }
 
