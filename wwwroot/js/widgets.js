6a9a565e2c1f683081ebf269d49237b28bf440b6
Joshua Wyckoff <joshua.wyckoff@okstate.edu>
2025-11-11 14:37:16 -0600

    Cross t's and dot i's. Should be ready for testing and merging with Master Branch.

--- wwwroot/js/widgets.js
+++ wwwroot/js/widgets.js
@@ -1,9 +1,6 @@
 /*
-* Note to Alex: There are two functions at the bottom for retrieving and saving data to the database. If you edit those, they should be called in the correct places.
-*/
-
-/*
 *   Initialize Global variables:
+*       'templateContainer' is the contianer for all template widgets (used for previewing and adding widgets)
 *       'container' is the dashboard container for all the widgets; all widgets are rendered within this object.
 *       'editSaveBtn' is the button that changes the 'draggable' state.
 *       'deleteBtns' is an array storing all delete buttone (used so that we can hide the delete buttons)
@@ -14,35 +11,32 @@
 const templateContainer = document.querySelector('.template_container');
 const container = document.querySelector('.dashboard_container');
 const editSaveBtn = document.getElementById('editSaveBtn');
-//var addBtns = documnet.querySelectorAll('.add-btn', {});
 var deleteBtns;
 var addBtns;
 var widgets = [];
 var draggies = [];
 var draggable = true;
 
-// Arrays of widget types and sizes used to display templates
-const widgetTypes = ['Test Widget'];
-const widgetSizes = [[1, 1], [1, 2], [2, 1], [2, 2]];
 
+
+// Set default widgets for testing (Commented out as it now pulls widgets from the database. Keeping for potentially setting default widgets later.)
 /*
-*   Set up the initial page rendering
-*       1) Get widget information from the database using 'getWidgets' function
-*       2) Render the widgets in HTML using the 'renderWidgets' function.
-*/
-
-// Set default widgets for testing
-// TODO: Once we can pull from database, we no longer need default widgets; delete following 7 lines of code.
-
 var defaultWidgets = [
     { userID: 1006, x: 64, y: 64, w: 2, h: 2, type: 'Test Widget' },
     { userID: 1006, x: 640, y: 64, w: 1, h: 1, type: 'Test Widget' },
     { userID: 1006, x: 640, y: 350, w: 1, h: 1, type: 'Test Widget' },
     { userID: 1006, x: 928, y: 64, w: 1, h: 2, type: 'Test Widget' }
 ];
-widgets = defaultWidgets;
+widgets = defaultWidgets;*/
 
-var widgetTemplates = [
+/*
+*   Set up the initial page rendering
+*       1) Set widget templates used when adding a widget to the dashboard
+*       2) Get widget information from the database using 'getWidgets' function
+*/
+
+// Set widget templates used when adding a widget to the dashboard
+const widgetTemplates = [
     { userID: 1006, x: 16, y: 16, w: 1, h: 1, type: 'Test Widget' },
     { userID: 1006, x: 16, y: 16, w: 1, h: 2, type: 'Test Widget' },
     { userID: 1006, x: 16, y: 16, w: 2, h: 1, type: 'Test Widget' },
@@ -50,21 +44,13 @@ var widgetTemplates = [
 ]
 
 // Get widget information from database
-//getWidgets()
-
-// Render the widgets in HTML
-renderWidgets();
-
-// Set the dashboard to be static
-toggleDraggble();
-
+getWidgets()
 
 /*
 *   Adds EventListener for the delete button. On Click:
 *       1) Save current widget positions
 *       2) Delete widget from 'widgets' array
-*       3) Save 'widgets' array to Database
-*       4) Re-Render the 'widgets' array
+*       3) Re-Render the 'widgets' array
 */
 container.addEventListener('click', (event) => {
     if (event.target.classList.contains('delete-btn')) {
@@ -77,9 +63,6 @@ container.addEventListener('click', (event) => {
         // Delete widget from 'widgets' array
         const index = parseInt(event.target.dataset.index);
         widgets.splice(index, 1);
-
-        // Save 'widgets' array to Database
-        //saveWidgets();
 
         // Re-Render the 'widgets' array
         renderWidgets();
@@ -108,6 +91,12 @@ function renderWidgets() {
         htmlString += '<div class="widget widget_card size-' + widgets[i].w + 'x' + widgets[i].h + '">' +
             '<button class="delete-btn" data-index="' + i + '">X</button>' +
             '<p>Drag Me Around!</p></div>';
+    }
+
+    if (htmlString == '') {
+        htmlString = "<h1>It looks like you don't have any widgets yet...</h1><p>Hit the Edit button at the top of the page, " +
+            "then scroll down to add widgets to get started!(If you are only seeing a Save button, that means you are alread in " +
+            "Edit Mode!)</p><p>Don't forget to hit Save when you're done!</p>";
     }
 
     // Set the HTML code of 'container' based on previous formatting
@@ -142,16 +131,18 @@ function renderWidgets() {
 /*
 *   Fucntion to toggle the draggable state of the widgets
 *       1) If they are currently draggable, then: 
-*           1) Disable draggabilly
-*           2) Change the 'editSaveBtn' to Edit
-*           3) Save the new positions to database.
+*           1) Disable 
+*           2) Save all widget positions locally
+*           3) Change the 'editSaveBtn' to Edit
+*           4) Save the new widget states to database.
 *
 *       2) If they are not currently draggable, then:
 *           1) Enable draggabilly
 *           2) Change the 'editSaveBtn' to Save.
 *
-*       3) Hide/Show delete buttons based on draggable status
-*       4) Toggle to boolean 'draggable' variable to track state.
+*       3) Hide/Show widget templates (for previewing and adding new widgets) based on draggable status
+*       4) Hide/Show delete buttons based on draggable status
+*       5) Toggle to boolean 'draggable' variable to track state.
 */
 function toggleDraggble() {
     if (draggable) {
@@ -160,14 +151,16 @@ function toggleDraggble() {
             // Disable draggabilly for each object
             draggies[i].disable();
 
-            // Update database with new positions
+            // Save widget position for each object
             widgets[i].x = draggies[i].position.x;
             widgets[i].y = draggies[i].position.y;
         }
 
-        // Update 'editSaveBtn'
+        // Update 'editSaveBtn' to Edit
         editSaveBtn.textContent = "Edit";
-        //saveWidgets();
+
+        // Save the current widget states to the database
+        saveWidgets();
     }
     else {
         // If not currently draggable
@@ -175,31 +168,57 @@ function toggleDraggble() {
             // Enable draggabilly for each object
             draggies[i].enable();
         }
-        // Update the 'editSaveButton'
+
+        // Update the 'editSaveButton' to Save
         editSaveBtn.textContent = "Save";
     }
 
+    // Hide/Show Widget Templates based on draggable status
     templateContainer.hidden = draggable;
 
+    // Hide/Show Delete Buttons based on draggable status
     for (var i = 0; i < deleteBtns.length; i++) {
         deleteBtns[i].hidden = draggable;
     }
+
+    // Toggle draggable state
     draggable = !draggable;
 } 
 
+/*
+*   Function to add widget to the widget array based on width, height, and type. 
+*   Called when '+' button is pressed on widget templates.
+*       1) Save existing widget positions locally
+*       2) Push new widget to local array of widgets
+*       3) Rerender widgets with new widget added
+*/
 function addWidget(width, height, widgetType) {
+    // Save existing widgets locally
     for (var i = 0; i < draggies.length; i++) {
         widgets[i].x = draggies[i].position.x;
         widgets[i].y = draggies[i].position.y;
     }
 
+    // Push new widget to widgets array
     widgets.push({ userID: 1006, x: 32, y: 32, w: width, h: height, type: widgetType });
+
+    // Render widgets
     renderWidgets();
 }
 
-// TODO: Write function to get the widgets. It doesn't need return, just set existing 'widgets' array to data from database.
+/*
+*   Function to get widgets from the database.
+*   Called upon loading the page.
+*   1) Gets widgets from the database in JSON format
+*   2) If successful:
+*       1) Replace local widgets array with data from database
+*       2) Rerender widgets
+*       3) Set draggable to false
+*   3) If not successful, throws error to console.
+*/
 async function getWidgets(/*userId*/) {
     try {
+        // Get widgets from te database in JSON format
         const response = await fetch(`/Dashboard?handler=Widgets&userId=1006`, {
             method: 'GET',
             headers: {
@@ -212,8 +231,8 @@ async function getWidgets(/*userId*/) {
         if (result.success) {
             console.log('Widgets loaded successfully:', result.widgets);
             widgets = result.widgets; // Replace current widgets array
-            renderWidgets(widgets); // Optional: your custom render function
-            //toggleDraggble();
+            renderWidgets(widgets); // Render Widgets
+            toggleDraggble(); // Set draggable to false
         } else {
             console.warn('Failed to load widgets:', result.message);
         }
@@ -222,7 +241,10 @@ async function getWidgets(/*userId*/) {
     }
 }
 
-// TODO: Write function to save widgets to the database (Alex, write save code here to simply save the entire widgets array to database)
+/*
+*   Function to save widgets to the database. 
+*   Called when 'Save' button is pressed.
+*/
 async function saveWidgets() {
     try {
         const userId = widgets[0].userID;
